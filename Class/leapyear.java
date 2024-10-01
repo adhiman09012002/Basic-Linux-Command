2136e6a6c2bbda3bc4e676c345e08091d8f54204
Ajay Dhiman <ajaydhiman09012002@gmail.com>
2024-10-01 15:30:04 +0530

    [Ajay] ADD: Basic linux command.

---
+++ Class/leapyear.java
@@ -0,0 +1,10 @@
+class Main{
+       public static void main(String [] args){
+         int n =2024;
+           if(n %400 == 0 || n %  4 == 0  && n % 100 !=0){
+             System.out.println("Year is a leap");
+            }else{
+               System.out.println("Year is not a  leap year");
+                 }
+                   }
+                      }
